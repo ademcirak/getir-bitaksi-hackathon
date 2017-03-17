b8c3ac549cff50d6b5218ef5af61f350cda991bb
adem-pc-ssd\adem <adem.cirak@gmail.com>
2017-03-17 18:55:36 +0300

    init

---
+++ README.md
@@ -0,0 +1,40 @@
+# getir-bitaksi-hackathon
+
+A  Node.js app for hackathon using [Hapi] (https://hapijs.com).
+
+## Running Locally
+
+Make sure you have [Node.js](http://nodejs.org/) installed.
+
+Create your .env file by copying .env.example file.
+
+```sh
+cp .env.example .env
+```
+
+Install dependencies and run!
+
+```sh
+git clone git@github.com:ademcirak/getir-bitaksi-hackathon.git
+cd getir-bitaksi-hackathon
+npm install
+npm start
+```
+
+Your app should now be running on [localhost:8080](http://localhost:8080/).
+
+## Deploying to Heroku
+
+```
+heroku create
+git push heroku master
+heroku open
+```
+
+Alternatively, you can deploy your own copy of the app using the web-based flow:
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+## Documentation
+
+Documentation LUL
---
+++ index.js
@@ -0,0 +1,55 @@
+const Hapi = require('hapi');
+const Boom = require('boom');
+require('dotenv').config();
+
+const dbOpts = {
+    url: process.env.MONGODB_CONNECTION_URL,
+    settings: {
+        poolSize: 10
+    },
+    decorate: true
+};
+
+const server = new Hapi.Server();
+server.connection({
+    port: process.env.SERVER_PORT,
+    host: process.env.SERVER_HOST
+});
+
+server.register({
+    register: require('hapi-mongodb'),
+    options: dbOpts
+}, function (err) {
+    if (err) {
+        console.error(err);
+        throw err;
+    }
+
+    server.route( {
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply({ message: 'Hello world!'});
+        }
+    });
+
+    server.route( {
+        method: 'GET',
+        path: '/getRecord',
+        handler: function(request, reply) {
+            const db = request.mongo.db;
+
+            db.collection('records').findOne({  key: 'chp8vgSkJDbyDKAS' }, function (err, result) {
+                if (err) {
+                    return reply(Boom.internal('Internal MongoDB error', err));
+                }
+
+                reply(result);
+            });
+        }
+    });
+
+    server.start(function() {
+        console.log('Server started at ' + server.info.uri);
+    });
+});
